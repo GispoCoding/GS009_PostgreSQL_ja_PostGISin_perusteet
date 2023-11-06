c60323c7683d31098e927fce1fae46cc21325438
Raine Ekman <122015055+RaineEkman@users.noreply.github.com>
2023-11-06 17:30:17 +0200

    Update initialize.sh
    Fix a script in the Docker image that lacks execute permissions and re-run it.

--- initialize.sh
+++ initialize.sh
@@ -2,3 +2,5 @@
 
 wget https://raw.githubusercontent.com/jpatokal/openflights/master/data/airports.dat -O data/client/airports.dat
 docker-compose run --rm client ./initializer.sh
+docker-compose exec dbhost /bin/chmod +x /docker-entrypoint-initdb.d/db_init.sh
+docker-compose exec dbhost /docker-entrypoint-initdb.d/db_init.sh
